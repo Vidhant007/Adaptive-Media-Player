a24e59c37bd8d5c79a70c8c13641dc9afa362cc7
Vidhant007 <thapavidhant@gmail.com>
2023-12-28 00:53:40 +0530

    Transcoding and Storage finished

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -25,7 +25,8 @@ const upload = multer({
 
 //get all movies
 const getMovies = async(req,res)=>{
-    res.send("Get Movies");
+    const movies = await MOVIE.find({title: 'CupHead'});
+    res.status(StatusCodes.OK).json(movies);
 }
 
 //this controller will run the transcode.sh script after videoUpload is completed
@@ -53,13 +54,27 @@ const addMovie = async (req, res) => {
             images: images,
             videoPath: videoPath,
         });
+        
+        const movieName = newMovie.title.replace(/\s+/g, '_');
+        const outputDir = path.join('videos', movieName);
+        
+        // Check if the directory exists; create it if not
+        if (!fs.existsSync(outputDir)) {
+            fs.mkdirSync(outputDir, { recursive: true });
+        }
 
         try {
             await newMovie.save();
 
             if(videoPath){
-                await RUNTRANSCODINGSCRIPT(videoPath, 'transcoded_videos');
+                await RUNTRANSCODINGSCRIPT(videoPath, outputDir);
+
+                // Update the videoPath in db with trancoded video path
+                newMovie.videoPath = outputDir;
+                await newMovie.save();
+
                 console.log('trancoding completed successfully')
+
             }
             res.status(StatusCodes.CREATED).json('Successfully uploaded');
         } catch (err) {
