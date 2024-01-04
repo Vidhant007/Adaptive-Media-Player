679a0cd2680e8c6d67cbc07e3a7392d2fd2d4e20
Vidhant007 <thapavidhant@gmail.com>
2024-01-04 18:09:49 +0530

    userCOntroller in progress

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,39 @@
-const User = require('../models/userModel');
 const {StatusCodes} = require('http-status-codes');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const PROFILE = require('../models/profileModel');
+const USER  = require('../models/userModel');
+const SUBSCRIPTION = require('../models/subscriptionModel');
 
 
 const registerUser = async (req,res)=>{
-    const {username,phone,email,password} = req.body;
-    const tempUser = {username,phone,password,email};
-    const user = await User.create({...tempUser});
-    res.status(StatusCodes.CREATED).send({user});
+    try{
+        const {username,phone,email,password} = req.body;
 
+        //checking if user is already exists
+        const existingUser = await USER.findOne({
+            $or: [
+              { username: username },
+              { phone: phone },
+              { email: email }
+            ]
+          });    
+
+
+          if (existingUser) {
+            return res.status(StatusCodes.BAD_REQUEST).json({ error: 'User already exists' });
+          }
+
+          
+
+
+
+
+        }catch(error){
+        console.log(error);
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error Creating User");
+    }
 }
 
 const loginUser = async(req,res)=>{
